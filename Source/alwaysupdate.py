5c023ba1b39bb5099ec2657db7575c9c6da40aa1
Vailla Rohit <rohitvailla@gmail.com>
2024-05-16 17:30:47 +0530

    Always update method

---
+++ Source/alwaysupdate.py
@@ -0,0 +1,22 @@
+from manim import *
+
+class Updaters(Scene):
+    def construct(self):
+
+        box = Rectangle(height=4, width=6, fill_color=BLUE, fill_opacity=0.55, stroke_color=WHITE)
+
+        text = MathTex("ln(2)")
+        
+        text.add_updater (lambda m: text.move_to(box.get_center()))
+
+        self.add(box, text)
+
+        self.play(box.animate.scale(0.5).to_edge(UL), run_time=3)
+
+        self.wait()
+
+        text.clear_updaters()
+
+        self.play(box.animate.to_edge(RIGHT), run_time=3)
+
+        self.wait()
