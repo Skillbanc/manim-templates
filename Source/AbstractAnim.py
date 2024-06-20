6e26db5960b88e46a5e75e6107eb3add6dce68ce
A Sindhu <asindhusri22@ifheindia.org>
2024-06-20 15:05:10 +0530

    Updated Abstractanim

--- Source/AbstractAnim.py
+++ Source/AbstractAnim.py
@@ -18,10 +18,13 @@ class AbstractAnim(Scene):
     colorChoice=[RED,BLUE,GREEN,PURPLE,ORANGE,YELLOW,LIGHT_PINK,WHITE,LIGHT_GRAY,LIGHT_BROWN,PINK,GRAY_BROWN]
     shapeChoice=[Circle,Triangle,Square,Rectangle]
     positionChoice = [[-6,-2,0],[4,-2,0],[2,0,0],[-6,2,0],[-4,-2,0],[-4,2,0],[-2,-2,0],[4,0,0],[-4,0,0],[-2,2,0],[2,-2,0],[-6,0,0],[2,2,0],[6,0,0],[4,2,0],[6,-2,0],[-2,0,0],[6,2,0]]
+    DeveloperList=""
+    SourceCodeFilename=""
+    
 
     # angleChoice = [TAU/5,TAU/4,TAU/3,TAU/2,-TAU/5,-TAU/4,-TAU/3,-TAU/2]
     isRandom = True
-    
+   
     def initChoices(self):
         self.colorChoice=[RED,BLUE,GREEN,PURPLE,ORANGE,YELLOW,LIGHT_PINK,WHITE,LIGHT_GRAY,LIGHT_BROWN,PINK,GRAY_BROWN]
         self.shapeChoice=[Circle,Triangle,Square,Rectangle]
@@ -152,7 +155,7 @@ class AbstractAnim(Scene):
             if len(cvo.onameList) == 0:
                 self.play(Create(arrow1),run_time=cvo.duration)
             #self.bring_to_back(arrow1)
-            grp1.add(arrow1)
+            #grp1.add(arrow1)
             
         if (len(cvo.onameList) > 0 and len(cvo.onameList) < 5):
             for index in range(len(cvo.onameList)):
@@ -161,7 +164,8 @@ class AbstractAnim(Scene):
                 onameLocalText = Tex(cvo.onameList[index],color=self.colorChoice[colorChoiceIndex]).scale(0.35).next_to(starLocal).shift(LEFT * .20)
                 arrow2 = CurvedArrow(cvoParent.pos,starLocal.get_center(),angle=cvo.angle)
                 self.play(Create(starLocal),Create(onameLocalText))#grpLocal.animate.move_to(cir1.get_center()).scale(0.5).shift(DOWN * 2))#scale(0.25))
-                self.play(Create(arrow2))
+                if cvoParent != cvo:
+                    self.play(Create(arrow2))
         else:
             self.play(grp1.animate.scale(1+0.1*len(cvo.onameList)))
             for index in range(len(cvo.onameList)):
@@ -170,7 +174,8 @@ class AbstractAnim(Scene):
                 onameLocalText = Tex(cvo.onameList[index],color=self.colorChoice[colorChoiceIndex]).scale(0.45).next_to(starLocal).shift(LEFT * .20)
                 arrow2 = CurvedArrow(cvoParent.pos,starLocal.get_center(),angle=cvo.angle)
                 self.play(Create(starLocal),Create(onameLocalText))#grpLocal.animate.move_to(cir1.get_center()).scale(0.5).shift(DOWN * 2))#scale(0.25))
-                self.play(Create(arrow2))
+                if cvoParent != cvo:
+                    self.play(Create(arrow2))
                           
         cvo.cnameMObject = cname
         cvo.onameMObject = oname
@@ -262,6 +267,37 @@ class AbstractAnim(Scene):
         self.play(self.logoGroup.animate.scale(0),run_time=1)
         # self.play(self.circles.animate.scale(0),lines.animate.scale(0),text.animate.scale(0),run_time=3)
         
+    def GithubSourceCodeReference(self): 
+        self.SetDeveloperList()
+        self.colorChoice=[BLUE,ORANGE,PINK,ORANGE,PURPLE]
+        p2 = cvo.CVO().CreateCVO("SOURCE CODE REFERENCE", "").setPosition([0,2.5,0])
+        p4 = cvo.CVO().CreateCVO("Github URL", "https://github.com/Skillbanc/manim-templates").setPosition([-4,1,0]).setangle(TAU / 3)
+        p5 = cvo.CVO().CreateCVO("File Name", self.SourceCodeFilename()).setPosition([4,1,0]).setangle(TAU / 3)
+        p6=cvo.CVO().CreateCVO("Architected By","Sudhakar Moparthy").setPosition([5,-2,0]).setangle(-TAU / 4)
+
+        p7=cvo.CVO().CreateCVO("Developed By",self.GetDeveloperList()).setPosition([0,-2,0]).setangle(-TAU / 4)
+        
+        p2.cvolist.append(p4)
+        p2.cvolist.append(p5)
+        self.setNumberOfCirclePositions(5)
+        p4.setcircleradius(3)
+        p5.setcircleradius(2)
+        p6.setcircleradius(1.5)
+        p2.cvolist.append(p6)
+        p2.cvolist.append(p7)
+        self.construct1(p2,p2)
+        
+    def GetDeveloperList(self): 
+        return self.DeveloperList
+      
+    def SetDeveloperList(self):  
+        pass  
+
+    def GetSourceCodeFilename(self):
+        return self.SourceCodeFilename
+    
+    def SetSourceCodeFilename(self):
+        pass
          
     def construct2(self,p10,cvoParent):  
         text0 = Tex(p10.onameList[0],color=BLUE)
@@ -278,7 +314,7 @@ class AbstractAnim(Scene):
             text01 = MathTex(p10.onameList[i],color=BLUE)
            else:   
             text1 = Tex(p10.onameList[i],color=BLUE)
-            text01 = MathTex(p10.onameList[i],color=BLUE)
+            text01 = Tex(p10.onameList[i],color=BLUE)
             
            
            self.play(grp1.animate.shift(UP * 1))
@@ -287,7 +323,7 @@ class AbstractAnim(Scene):
            grp1.add(text01)
            text0 = text1
            # self.wait(2)
-          
+         
            
            
         self.wait(2)
