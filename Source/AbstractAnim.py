0443b2032cf000711fed7608efd64b3976546960
Hriday Bhushan <hridaybhushan@gmail.com>
2024-06-20 20:58:56 +0530

    Updated

--- Source/AbstractAnim.py
+++ Source/AbstractAnim.py
@@ -20,7 +20,6 @@ class AbstractAnim(Scene):
     positionChoice = [[-6,-2,0],[4,-2,0],[2,0,0],[-6,2,0],[-4,-2,0],[-4,2,0],[-2,-2,0],[4,0,0],[-4,0,0],[-2,2,0],[2,-2,0],[-6,0,0],[2,2,0],[6,0,0],[4,2,0],[6,-2,0],[-2,0,0],[6,2,0]]
     DeveloperList=""
     SourceCodeFileName=""
-    
 
     # angleChoice = [TAU/5,TAU/4,TAU/3,TAU/2,-TAU/5,-TAU/4,-TAU/3,-TAU/2]
     isRandom = True
@@ -164,8 +163,7 @@ class AbstractAnim(Scene):
                 onameLocalText = Tex(cvo.onameList[index],color=self.colorChoice[colorChoiceIndex]).scale(0.35).next_to(starLocal).shift(LEFT * .20)
                 arrow2 = CurvedArrow(cvoParent.pos,starLocal.get_center(),angle=cvo.angle)
                 self.play(Create(starLocal),Create(onameLocalText))#grpLocal.animate.move_to(cir1.get_center()).scale(0.5).shift(DOWN * 2))#scale(0.25))
-                if cvoParent != cvo:
-                    self.play(Create(arrow2))
+                self.play(Create(arrow2))
         else:
             self.play(grp1.animate.scale(1+0.1*len(cvo.onameList)))
             for index in range(len(cvo.onameList)):
@@ -174,8 +172,7 @@ class AbstractAnim(Scene):
                 onameLocalText = Tex(cvo.onameList[index],color=self.colorChoice[colorChoiceIndex]).scale(0.45).next_to(starLocal).shift(LEFT * .20)
                 arrow2 = CurvedArrow(cvoParent.pos,starLocal.get_center(),angle=cvo.angle)
                 self.play(Create(starLocal),Create(onameLocalText))#grpLocal.animate.move_to(cir1.get_center()).scale(0.5).shift(DOWN * 2))#scale(0.25))
-                if cvoParent != cvo:
-                    self.play(Create(arrow2))
+                self.play(Create(arrow2))
                           
         cvo.cnameMObject = cname
         cvo.onameMObject = oname
@@ -275,7 +272,7 @@ class AbstractAnim(Scene):
         p4 = cvo.CVO().CreateCVO("Github URL", "https://github.com/Skillbanc/manim-templates").setPosition([-4,1,0]).setangle(TAU / 3)
         p5 = cvo.CVO().CreateCVO("File Name", self.GetSourceCodeFileName()).setPosition([4,1,0]).setangle(TAU / 3)
         p6=cvo.CVO().CreateCVO("Architected By","Sudhakar Moparthy").setPosition([5,-2,0]).setangle(-TAU / 4)
-
+        p6.extendOname=['Sudhakar Moparthy','Vailla Rohit']
         p7=cvo.CVO().CreateCVO("Developed By",self.GetDeveloperList()).setPosition([0,-2,0]).setangle(-TAU / 4)
         
         p2.cvolist.append(p4)
@@ -290,15 +287,16 @@ class AbstractAnim(Scene):
         
     def GetDeveloperList(self): 
         return self.DeveloperList
+    
+    def GetSourceCodeFileName(self):
+        return self.SourceCodeFileName
+
+
+    def SetSourceCodeFileName(self):
+        pass
       
     def SetDeveloperList(self):  
         pass  
-
-    def GetSourceCodeFileName(self):
-        return self.SourceCodeFileName
-    
-    def SetSourceCodeFileName(self):
-        pass
          
     def construct2(self,p10,cvoParent):  
         text0 = Tex(p10.onameList[0],color=BLUE)
