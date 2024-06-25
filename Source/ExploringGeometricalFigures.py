0b261c18d01de655a615fa5bcf80c5dda2ef4274
galiudaykiran <168338373+galiudaykiran@users.noreply.github.com>
2024-06-25 19:29:34 +0530

    Update ExploringGeometricalFigures.py

--- Source/ExploringGeometricalFigures.py
+++ Source/ExploringGeometricalFigures.py
@@ -41,6 +41,7 @@ class ExploringGeometricalFigures(AbstractAnim):
         self.fadeOutCurrentScene()
         self.pointsymmetry()
         self.fadeOutCurrentScene()
+        self.GithubSourceCodeReference()
 
 
 
@@ -311,7 +312,7 @@ class ExploringGeometricalFigures(AbstractAnim):
          p10.cvolist.append(p11)
          p10.cvolist.append(p13)
          p10.cvolist.append(p14)
-
+        
 
          p11.setcircleradius(1.5)
          p12.setcircleradius(1.5)
@@ -575,60 +576,14 @@ class ExploringGeometricalFigures(AbstractAnim):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#SKILLBANC LOGO
-
-    def RenderSkillbancLogo(self):
         
-        self.orange = "#D76800"
-        self.blue = "#3F64A7"
-        self.green = "#96D24D"
-        self.circles = VGroup(
-            Circle(radius=1).rotate(PI/2).set_fill(self.orange, opacity=1).set_stroke(self.orange, opacity=1).shift(LEFT*3),
-            Circle(radius=1).rotate(PI/2).set_fill(self.blue, opacity=1).set_stroke(self.blue, opacity=1),
-            Circle(radius=1).rotate(PI/2).set_fill(self.green, opacity=1).set_stroke(self.green, opacity=1).shift(RIGHT*3)
-        )
-        circle1, circle2, circle3 = self.circles
-        
-        lines = VGroup(
-            Arrow(circle1.get_right(), circle2.get_left(), max_tip_length_to_length_ratio=2),
-            Arrow(circle2.get_right(), circle3.get_left(), max_tip_length_to_length_ratio=2),
-            CurvedArrow(circle1.get_top(), circle3.get_top(), angle=-TAU/4),
-            CurvedArrow(circle1.get_bottom(), circle3.get_bottom(), angle=TAU/4)
-        )
+    def SetDeveloperList(self):
+        self.DeveloperList = "Uday Kiran"   
 
-        # Adjust the starting points of the straight arrows to touch the circumference
-        lines[0].put_start_and_end_on(circle1.get_right(), circle2.get_left())
-        lines[1].put_start_and_end_on(circle2.get_right(), circle3.get_left())
-
-        # Add the text "skillbanc" below the curved arrows
-        self.play(Create(circle1), Create(circle2), Create(circle3), rate_func=smooth, run_time=1)
-        self.play(Create(lines), rate_func=smooth, run_time=1)
-        text = Text("Skillbanc.com, Inc.").next_to(lines[3], DOWN)
-        self.play(Create(text), rate_func=smooth, run_time=0.75)
+    def SetSourceCodeFileName(self):
+        self.SourceCodeFileName="ExploringGeometricalFigures.py"      
         
-        self.logoGroup = VGroup().add(self.circles).add(lines).add(text)
-        self.play(self.logoGroup.animate.scale(0),run_time=1)
-        # self.play(self.circles.animate.scale(0),lines.animate.scale(0),text.animate.scale(0),run_time=3)
-        
-
+     
       
 if __name__ == "__main__":
     scene = ExploringGeometricalFigures()
