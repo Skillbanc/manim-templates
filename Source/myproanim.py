d74c94c14a4d17a394b4e41885bf90b513ff3c71
Vailla Rohit <rohitvailla@gmail.com>
2024-06-27 12:05:32 +0530

    Filtered

--- Source/myproanim.py
+++
@@ -1,8 +0,0 @@
-from venv import create
-from manim import *
-
-class createcircle():
-    def construct(self):
-        circle=circle()
-        circle.set_fill(PINK, optacity=0.5)
-        self.play(create(circle))
