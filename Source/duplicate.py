14d59e198f7a39fe9277c926baeab92d07380b59
Raghu Varma <bommisettyraghu22@ifheindia.org>
2024-07-01 14:18:06 +0530

    01-07-20242:17 pm

--- Source/duplicate.py
+++ Source/duplicate.py
@@ -1,24 +1,51 @@
-from AbstractAnim import AbstractAnim
 from manim import *
+from AbstractAnim import AbstractAnim
+import cvo
 
 
-class duplicate(AbstractAnim):
+class TableScene(AbstractAnim):
     def construct(self):
+        self.InterestingPatternsofSquare()
+        
+    
+    
+    def InterestingPatternsofSquare(self):
 
-        # Title
-        title = Text("Places Table", font_size=24)
-        title.to_edge(UP)
-        self.play(Write(title))
+        self.setNumberOfCirclePositions(7)
+        self.colorChoice=[RED,BLUE,PURPLE,YELLOW,GREEN,ORANGE,PINK]
 
-        # Table data
-        headers = ["Places", "Ten Crores\n(T. Cr)", "Crores\n(Cr)", "Ten Lakhs\n(T. La)", "Lakhs\n(La)", "Ten Thousands\n(T. Th.)", "Thousands\n(Th.)", "Hundreds\n(H)", "Tens\n(T)", "Ones\n(O)"]
-        row1 = ["Number", "10,00,00,000", "1,00,00,000", "10,00,000", "1,00,000", "10,000", "1,000", "100", "10", "1"]
-        row2 = ["No. of Digits", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+        #  p1=cvo.CVO().CreateCVO("o1name","o2name","c1name","c2name")
+        p10=cvo.CVO().CreateCVO("Interesting patterns of Square","Chapter:6.2").setPosition([0,2.5,0])
+        p11=cvo.CVO().CreateCVO("Sum of first n odd numbers = $n^2$","$\sum_{i=1}^{n} (2i-1) = n^2$").setPosition([-4.12,1,0]).setangle(TAU/3)
+        p12=cvo.CVO().CreateCVO("Numeric Palindrome","$1111^2$=1234321").setPosition([0,0,0]).setangle(-TAU/4)
+        p13=cvo.CVO().CreateCVO("Numbers between sucessive squares","2*Base of the first Number").setPosition([4,1,0]).setangle(-TAU/3)
+        p14=cvo.CVO().CreateCVO("Example","1+3+5+7=16=$4^2$").setPosition([-2.25,-2,0])
+        p15=cvo.CVO().CreateCVO("Example","$1001^2$=1002001").setPosition([0,-2,0])
+        p16=cvo.CVO().CreateCVO("Example","$2^2$=4;$3^2$=9=2*2=4").setPosition([2.25,-2,0])
+        p10.cvolist.append(p11)
+        p10.cvolist.append(p12)
+        p10.cvolist.append(p13)
+        p11.cvolist.append(p14)
+        p12.cvolist.append(p15)
+        p13.cvolist.append(p16)
+ 
+    
+        p11.setcircleradius(1.25)
+        p12.setcircleradius(1.05)
+        p14.setcircleradius(1.25)
+        p15.setcircleradius(1.1)
+        p16.setcircleradius(1.25)
 
-        # Combine headers and rows
-        table_data = [headers, row1, row2]
+ 
+        self.construct1(p10,p10)
+        #self.play()
+
+
+    
+
+    
+
 
 if __name__ == "__main__":
-    from manim import *
-    scene = duplicate()
+    scene = TableScene()
     scene.render()
