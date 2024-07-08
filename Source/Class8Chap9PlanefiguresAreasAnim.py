bb7fa19a9f432dccf95919dfddaf31a63e5fc2e1
lasyanannapaneni <nannapanenilasyamadhuri22@ifheindia.org>
2024-07-08 16:49:00 +0530

    idk

--- Source/Class8Chap9PlanefiguresAreasAnim.py
+++ Source/Class8Chap9PlanefiguresAreasAnim.py
@@ -5,35 +5,42 @@ from AbstractAnim import AbstractAnim
 
 import cvo
 
-class Class8Chap9PlanefiguresAreasAnim(AbstractAnim):
+class Grade8Chapter9PlanefiguresAreasAnim(AbstractAnim):
 
     def construct(self):
-        # self.RenderSkillbancLogo()
-        # self.Intro()
-        # self.fadeOutCurrentScene()
-        # self.shape1()
-        # self.fadeOutCurrentScene()
-        # self.shape2()
-        # self.fadeOutCurrentScene()
-        # self.shape3()
-        # self.fadeOutCurrentScene()
-        # self.shape4()
-        # self.fadeOutCurrentScene()
-        # self.shape5()
-        # self.fadeOutCurrentScene()
-        # self.shape6()
-        # self.fadeOutCurrentScene()
-        # self.shape7()
-        # self.fadeOutCurrentScene()
+        self.RenderSkillbancLogo()
+        self.Intro()
+        self.fadeOutCurrentScene()
+        self.shape1()
+        self.fadeOutCurrentScene()
+        self.shape2()
+        self.fadeOutCurrentScene()
+        self.shape3()
+        self.fadeOutCurrentScene()
+        self.shape4()
+        self.fadeOutCurrentScene()
+        self.shape5()
+        self.fadeOutCurrentScene()
+        self.shape6()
+        self.fadeOutCurrentScene()
+        self.shape7()
+        self.fadeOutCurrentScene()
         self.quad()
-        # self.GithubSourceCodeReference()
+        self.fadeOutCurrentScene()
+        self.poly()
+        self.fadeOutCurrentScene()
+        self.cirpath()
+        self.fadeOutCurrentScene()
+        self.sector()
+        self.fadeOutCurrentScene()
+        self.GithubSourceCodeReference()
     
 
     def SetDeveloperList(self):
         self.DeveloperList = "Lasya Nannapaneni"
 
     def SetSourceCodeFileName(self):
-        self.SourceCodeFileName = "Class8Chap9PlanefiguresAreasAnim.py"    
+        self.SourceCodeFileName = "Grade8Chapter9PlanefiguresAreasAnim.py"    
 
 
     def Intro(self):
@@ -365,7 +372,7 @@ class Class8Chap9PlanefiguresAreasAnim(AbstractAnim):
 
     def quad(self):
         
-        self.play(Write(NumberPlane()))
+        # self.play(Write(NumberPlane()))
         title = Text("Area of a Quadrilateral",font_size=45)
         title.move_to([-3,3.3,0])
         u = Underline(title)
@@ -402,14 +409,310 @@ class Class8Chap9PlanefiguresAreasAnim(AbstractAnim):
         d2 = DashedLine(start=[5.1,0.6,0],end=[4.4,1.7,0],color=BLUE)
         self.play(Write(d2))
 
-        a1 = ((3,1.1,0),(3.1,1,0))
+        a1 = Text("h1",font_size=25,color=ORANGE).move_to([3,1.8,0])
+        a2 = Text("h2",font_size=25,color=ORANGE).move_to([4.3,1,0])
+        self.play(Write(a1))
+        self.play(Write(a2))
+        self.fadeOutCurrentScene()
 
 
+        are =  Text("Calculating the area of quadrilateral",font_size=40).move_to([-2.5,3,0])
+        un = Underline(are)
+        self.play(Write(are))
+        self.play(Write(un))
+
+
+        formula_steps = [
+            MathTex(r"Area \ of \ the \ quadrilateral ABCD =  (Area \ of \ \triangle ABC) + (Area \ of \ \triangle ADC)", font_size=40, color=WHITE).shift(UP),
             
+            MathTex(r"= ((\frac{1}{2} * AC * h1) + (\frac{1}{2} * AC * h2))",font_size=40,color=WHITE),
+            MathTex(r"= \frac{1}{2} AC(h1 + h2)",font_size=40,color=WHITE).shift(DOWN),
+            MathTex(r"Area \ of \ quadrilateral ABCD = \frac{1}{2} d(h1 + h2),",font_size=40,color=WHITE).shift(DOWN*2),
+            MathTex(r"where \ 'd' \ denotes \ the \ length \ of \ the \ diagonal AC",font_size=40,color=WHITE).shift(DOWN*3)
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)
 
 
+
+    def poly(self):
+
+        title = Text("Area of a Polygon",font_size=45)
+        title.move_to([-3,3.3,0])
+        u = Underline(title)
+        subtil = Text("The area of a polygon may be obtained by dividing\n\n"
+                      "the polygon into a number of simple shapes \n\n"
+                      "(triangles, rectangles etc.) Then the areas of \n\n"
+                       "each of them can be calculated and added up \n\n"
+                       "to get the required area.",font_size=25)
+        subtil.move_to([-2.25,0.5,0])
+        self.play(Write(title))
+        self.play(Write(u))
+        self.play(Write(subtil,run_time=6))
+
+        a = ((4.6,3.3,0),(6.3,1.5,0),(5.6,-0.2,0),(4.6,-1.3,0),(2.3,0.5,0),(4.6,3.3,0))
+        ex1 = Polygon(*a,color=WHITE,stroke_width=5)
+        self.play(Write(ex1))
+        self.wait()
+
+        s1= Text("A",font_size=30).move_to([4.6,-1.6,0])
+        s2= Text("B",font_size=30).move_to([5.9,-0.2,0])
+        s3= Text("C",font_size=30).move_to([6.6,1.5,0])
+        s4= Text("D",font_size=30).move_to([4.6,3.7,0])
+        s5= Text("E",font_size=30).move_to([1.9,0.5,0])
+        self.play(Write(s1))
+        self.play(Write(s2))
+        self.play(Write(s3))
+        self.play(Write(s4))
+        self.play(Write(s5))
+
+
+
+        d1 = Line(start=[4.6,-1.3,0],end=[4.6,3.3,0],color=BLUE)
+        self.play(Write(d1))
+
+        d2 = Line(start=[6.3,1.5,0],end=[4.6,1.5,0],color=BLUE)
+        self.play(Write(d2))
+
+        d3 = Line(start=[5.6,-0.2,0],end=[4.6,-0.2,0],color=BLUE)
+        self.play(Write(d3))
+
+        d4 = Line(start=[2.3,0.5,0],end=[4.6,0.5,0],color=BLUE)
+        self.play(Write(d4))
+
+        s6= Text("F",font_size=30).move_to([4.2,1.5,0])
+        s7= Text("G",font_size=30).move_to([4.8,0.5,0])
+        s8= Text("H",font_size=30).move_to([4.3,-0.2,0])
+        self.play(Write(s6))
+        self.play(Write(s7))
+        self.play(Write(s8))
+
+        a1 = Text("50",font_size=25,color=ORANGE).move_to([4.4,2.3,0])
+        a2 = Text("50",font_size=25,color=ORANGE).move_to([5.3,1.8,0])
+        a3 = Text("60",font_size=25,color=ORANGE).move_to([3.5,0.7,0])
+        a4 = Text("40",font_size=25,color=ORANGE).move_to([4.3,1,0])
+        a5 = Text("15",font_size=25,color=ORANGE).move_to([4.3,0.2,0])
+        a6 = Text("25",font_size=25,color=ORANGE).move_to([4.3,-0.7,0])
+        a7 = Text("25",font_size=25,color=ORANGE).move_to([5.1,0,0])
+        self.play(Write(a1))
+        self.play(Write(a2))
+        self.play(Write(a3))
+        self.play(Write(a4))
+        self.play(Write(a5))
+        self.play(Write(a6))
+        self.play(Write(a7))
+        self.fadeOutCurrentScene()
+
+
+        are =  Text("Calculating the area of given polygon",font_size=40).move_to([-2.3,3,0])
+        un = Underline(are)
+        self.play(Write(are))
+        self.play(Write(un))
+
+
+        formula_steps = [
+            MathTex(r"Area \ of \ ABCDE = Area \ of \ \triangle ABH + Area \ of \ trap \ BCFH", font_size=40, color=WHITE).shift(UP*1.5),
+            MathTex(r"+ Area \ of \ \triangle CDF + Area \ of \ \triangle AED", font_size=40, color=WHITE).shift(UP),
+            MathTex(r"Now, Area \ of \ \triangle ABH",font_size=40,color=WHITE).shift(LEFT*3),
+            MathTex(r"= \frac{1}{2}*AH*HB",font_size=40,color=WHITE).shift(DOWN),
+            MathTex(r"=\frac{1}{2}*25*25",font_size=40,color=WHITE).shift(DOWN*2),
+            MathTex(r"=\frac{652}{2} \ m^2 = 312.5 \ m^2",font_size=40,color=WHITE).shift(DOWN*3)
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)
+
+        self.fadeOutCurrentScene()    
+
+        
+        formula_steps = [
+            MathTex(r"Area \ of \ trap \ BCFH =\ \frac{1}{2}*(HB + FC)*HF ", font_size=40, color=WHITE).shift(UP*3 + LEFT*2.5),
+            MathTex(r"=\ \frac{1}{2}*(25 + 50)*55 \ m^2 ", font_size=40, color=WHITE).shift(UP*2),
+            MathTex(r"=\ \frac{75*55}{2} \ m^2\ =2062.5\ m^2",font_size=40,color=WHITE).shift(UP),
+            MathTex(r"Area \ of \ \triangle CDF =\ \frac{1}{2}*FC*DF",font_size=40,color=WHITE).shift(DOWN + LEFT*2.5),
+            MathTex(r"=\ \frac{1}{2}*50*50\ m^2 ",font_size=40,color=WHITE).shift(DOWN*2),
+            MathTex(r"=1250\ m^2",font_size=40,color=WHITE).shift(DOWN*3)
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)
+
+
+        self.fadeOutCurrentScene()    
+
+        
+        formula_steps = [
+            MathTex(r"Area \ of \ \triangle AED =\ \frac{1}{2}*AD*EG", font_size=40, color=WHITE).shift(UP*3 + LEFT*2.5),
+            MathTex(r"=\ \frac{1}{2}*130*60\ m^2", font_size=40, color=WHITE).shift(UP*2),
+            MathTex(r"=3900\ m^2",font_size=40,color=WHITE).shift(UP),
+            MathTex(r"Thus,\ area\ of\ ABCDE ",font_size=40,color=WHITE).shift(DOWN + LEFT*2.5),
+            MathTex(r"=312.5\ m^2 +2062.5\ m^2 + 1250\ m^2 + 3900\ m^2 ",font_size=40,color=WHITE).shift(DOWN*2),
+            MathTex(r"=7525\ m^2",font_size=40,color=WHITE).shift(DOWN*3)
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)    
+
+
+    def cirpath(self):
+        
+
+        title = Text("Area of a Circular Path or Ring",font_size=45).move_to([-2.7,3.3,0])
+        u = Underline(title)
+        subtil = Text("The Area of the circular path is the difference\n\n"
+                      "of Area of outer circle and inner circle.",font_size=28).move_to([-2.25,2,0])
+        self.play(Write(title))
+        self.play(Write(u))
+        self.play(Write(subtil,run_time=6))
+
+        c1 = Circle(radius=2,color=GREEN,fill_color=GREEN,fill_opacity=0.6).move_to([4,-1.5,0])
+        self.play(Write(c1))
+        c2 = Circle(radius=1,color=GREEN,fill_color=BLACK,fill_opacity=1).move_to([4,-1.5,0])
+        self.play(Write(c2))
+        r1 = Line(start=[4,-1.5,0],end=[4,0.5,0],color=WHITE)
+        t1= Text("R",font_size=28).move_to([3.6,0,0])
+        self.play(Write(r1))
+        self.play(Write(t1))
+        r2 = Line(start=[4,-1.5,0],end=[5,-1.5,0],color=WHITE)
+        t2= Text("r",font_size=28).move_to([4.5,-1.2,0])
+        self.play(Write(r2))
+        self.play(Write(t2))
+
+        self.fadeOutCurrentScene() 
+
+        are =  Text("Calculating the area of circular path",font_size=40).move_to([-2.3,3,0])
+        un = Underline(are)
+        self.play(Write(are))
+        self.play(Write(un))
+
+        formula_steps = [
+            MathTex(r"Area \ of \ circular \ path \ where,\ R=10cm, \ r=4cm.", font_size=40, color=WHITE).shift(UP*1.5),
+            MathTex(r"(i) Find\ the\ area\ of\ larger\ circle\ with\ radius\ R", font_size=40, color=WHITE).shift(UP),
+            MathTex(r"(ii) Find\ the\ area\ of\ smaller\ circle\ with\ radius\ r",font_size=40,color=WHITE),
+            MathTex(r"(iii) Find\ the\ area\ of\ circular\ path\ (where\ \pi =3.14)",font_size=40,color=WHITE).shift(DOWN),           
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)
+
+        self.fadeOutCurrentScene()
+        
+        formula_steps = [
+            MathTex(r"Radius\ of\ the\ larger\ circle\ =\ 10cm", font_size=40, color=WHITE).shift(UP*3 + LEFT*2.5),
+            MathTex(r"So,area\ of\ the\ larger\ circle=\ \pi R^2", font_size=40, color=WHITE).shift(UP*2),
+            MathTex(r"=3.14*10*10\ =\ 314cm^2",font_size=40,color=WHITE).shift(UP),
+            MathTex(r"Radius\ of\ the\ smaller\ circle\ =\ 4cm",font_size=40,color=WHITE).shift(DOWN + LEFT*2.5),
+            MathTex(r"So,area\ of\ the\ smaller\ circle=\ \pi r^2",font_size=40,color=WHITE).shift(DOWN*2),
+            MathTex(r"=3.14*4*4\ =\ 50.24cm^2",font_size=40,color=WHITE).shift(DOWN*3)
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)  
+
+        self.fadeOutCurrentScene()
+        
+        formula_steps = [
+            MathTex(r"Area\ of\ the\ circular\ path\ =\ ", font_size=40, color=WHITE).shift(UP*3 + LEFT*2.5),
+            MathTex(r"Area\ of\ the\ larger\ circle-\ Area\ of\ the\ smaller\ circle", font_size=40, color=WHITE).shift(UP*2 + LEFT),
+            MathTex(r"=(314\ -\ 50.24)cm^2",font_size=40,color=WHITE).shift(UP),
+            MathTex(r"=\ 263.76cm^2",font_size=40,color=WHITE),
+        ]
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)      
+
+ 
+    def sector(self):
+
+        # self.play(Write(NumberPlane()))
+
+        title = Text("Area of Sector",font_size=45).move_to([-3.5,3.3,0])
+        u = Underline(title)
+        subtil = Text("A part of a circle bounded by two radii\n\n" 
+                      "and an arc is called sector.\n\n",font_size=28).move_to([-2.25,2,0])
+        self.play(Write(title))
+        self.play(Write(u))
+        self.play(Write(subtil,run_time=6))
+
+
+        c1 = Circle(radius=2,color=WHITE).move_to([4,1,0])
+        self.play(Write(c1))
+        r1 = Line(start=[4,1,0],end=[3,-0.7,0],color=RED)
+        t1= Text("r",font_size=28).move_to([3.2,0,0])
+        self.play(Write(r1))
+        self.play(Write(t1))
+        r2 = Line(start=[4,1,0],end=[5,-0.7,0],color=RED)
+        t2= Text("r",font_size=28).move_to([4.8,0,0])
+        t3= Text("O",font_size=28).move_to([4,1.3,0])
+        self.play(Write(r2))
+        p0 = [3,-0.7,0]
+        p00 = [5,-0.7,0]
+        a0 = ArcBetweenPoints(p0,p00,color=RED,radius=2)
+        self.play(Write(t2))
+        self.play(Write(t3))
+        self.play(Write(a0))
+
+        
+        p1 = [3,-1,0]
+        p2 = [5,-1,0]
+        a1 = ArcBetweenPoints(p1,p2,radius=2)
+        self.play(Write(a1))
+        t4= Text("A",font_size=28).move_to([2.8,-1,0])
+        t5= Text("B",font_size=28).move_to([5.2,-1,0])
+        t6= Text("l",font_size=28).move_to([4,-2,0])
+        self.play(Write(t4))
+        self.play(Write(t5))
+        self.play(Write(t6))
+
+        p3 = [3.8,0.7,0]
+        p4 = [4.2,0.7,0]
+        a2 = ArcBetweenPoints(p3,p4)
+        self.play(Write(a2))
+        tt = MathTex(r"x^\circ",font_size=30).move_to([4,0.3,0])
+        self.play(Write(tt))
+
+        self.fadeOutCurrentScene()
+
+
+        
+        formula_steps = [
+            MathTex(r"\small The\ Area\ of\ a\ circle\ with\ radius\ r = \pi r^2", font_size=40, color=WHITE).shift(UP*2.2),
+            MathTex(r"\\ \small Angle\ subtended\ by\ the\ arc\ of\ the\ sector\ at\ centre\ of\ the\ circle\ is\ x^\circ", font_size=40, color=WHITE).shift(UP),
+            MathTex(r"Area\ of\ a\ sector\ and\ its\ angle\ are\ in\ direct\ proportion",font_size=40,color=WHITE),
+            MathTex(r"Area\ of\ sector\ :\ Area\ of\ circle = x^\circ\ :\ 360^\circ",font_size=40,color=WHITE).shift(DOWN),           
+            MathTex(r"Area\ of\ sector\ OAB\ = \frac{x^\circ}{360^\circ}*Area\ of\ the\ circle",font_size=40,color=WHITE).shift(DOWN*2),           
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)
+
+
+        self.fadeOutCurrentScene()
+
+
+        formula_steps = [
+            MathTex(r"Hence,\ Area\ of\ sector\ OAB = \frac{x^\circ}{360^\circ}* \pi r^2\ \ \ \ \ [\pi r^2 = \pi r *\frac{2r}{r}] ", font_size=40, color=WHITE).shift(UP*3 + LEFT*1.2),
+            MathTex(r"= \frac{x^\circ}{360^\circ}*2 \pi r * \frac{r}{2}", font_size=40, color=WHITE).shift(UP*2),
+            MathTex(r"=l *\frac{r}{2}",font_size=40,color=WHITE).shift(UP),
+            MathTex(r"=\frac{lr}{2} \ \ \ [l\ is\ length\ of\ the\ arc]",font_size=40,color=WHITE),
+            
+        ]
+         
+        for step in formula_steps:
+            self.play(Write(step))
+            self.wait(2)
+            
+ 
 
 
 if __name__ == "__main__":
-    scene = Class8Chap9PlanefiguresAreasAnim()
+    scene = Grade8Chapter9PlanefiguresAreasAnim()
     scene.render()
